922ac6109b22a46bc63a82006c5ccd94ba6054e0
Paula Rodrigues <pullynnhah@icloud.com>
2022-12-29 19:11:08 -0300

    change folder structure and react first lec

---
+++ modulo_02/semana_01/ex_01/src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Lista de Compras</h1>
+      <ul>
+        <li>Pão</li>
+        <li>Banana</li>
+        <li>Milk Shake de Doce de Leite</li>
+        <li>Nutella</li>
+        <li>Sorvete</li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector(".root"));
